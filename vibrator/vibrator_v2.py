1e8ec0e065d1cfd664a445bd3ed0d7732f4f4535
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2022-05-02 00:12:21 +0300

    bag vibrator_v2

--- vibrator/vibrator_v2.py
+++ vibrator/vibrator_v2.py
@@ -1,18 +1,13 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from plyer import vibrator
+from plyer.vibrator import vibrator
 
 Builder.load_file('vibrator.kv')
 
 class VibrationInterface(BoxLayout):
     '''Root Widget.'''
-    pass
-
-class VibrationApp(App):
-    def build(self):
-        return VibrationInterface()
-
+    
     def vibro(self, n):
         vibrator.vibrate(n)
 
@@ -20,10 +15,15 @@ class VibrationApp(App):
         vibrator.cancel()
 
     def pattern(self):
-        vibrator.pattern([float(n) for n in self.root.ti.text.split(',')])
+        # vibrator.pattern([float(n) for n in self.root.ids.ti.text.split(',')])
+        vibrator.pattern([float(n) for n in self.ids.ti.text.split(',')])
 
     def exists(self):
         vibrator.exists()
+
+class VibrationApp(App):
+    def build(self):
+        return VibrationInterface()
 
     def on_pause(self):
         return True
