6637cc89dbde5a9d9a21a2bf773935efd54246cc
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2022-05-01 23:30:00 +0300

    fix-2 main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from kivy.app import App
-from vibrator.vibrator_v1 import VibrationApp
+# from vibrator.vibrator_v1 import VibrationApp
+from vibrator.vibrator_v2 import VibrationApp
 VibrationApp().run()
