5f668f336c4f84a365e0318d04d52369fd2405a6
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2022-05-04 21:02:31 +0300

    test vibrator_v3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
 # from vibrator.vibrator_v1 import VibrationApp
-from vibrator.vibrator_v2 import VibrationApp
+# from vibrator.vibrator_v2 import VibrationApp
+from vibrator.vibrator_v3 import VibrationApp
 VibrationApp().run()
