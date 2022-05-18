77eb88094539e66c8b908846f657db47bfeec11b
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-05-18 15:47:34 +0300

    test show_files_dir

--- access/access.py
+++ access/access.py
@@ -16,7 +16,7 @@ Builder.load_file(join(dirname(__file__), 'main.kv'))
 # listdir - файлы в текущей директории
 import os
 from os import listdir
-# импортируем молуль stat
+# импортируем молуль stat (работа с разрешениями прав доступа файлов и папок)
 import stat
 # *****************************************************************************************
 class Main(BoxLayout):
@@ -26,32 +26,49 @@ class Main(BoxLayout):
     # показать все файлы в текущей директории с установленными разрешениями
     # отсортировано по возрастанию
     def show_files_dir(self):
+        # константы
+        # величина выравнивания
+        delta = int(10)
+
         # получаем все файлы и папки в текущей директории и сортируем
         # self.ids.text_input_main.text = '\n'.join(listdir())
         files_arr = sorted(listdir())
+
+        # выравниваем текст в files_arr_ljust
+        files_arr_ljust = list()
+        for i in files_arr:
+            files_arr_ljust.append(i.ljust(delta, '*'))
+
         # массив установленныз разрешений для файлов и папок
         access_arr = list()
         # заполняем массив установленныз разрешений для файлов и папок
         for i in files_arr:
-            # Определим установленные разрешения
-            access_arr.append(stat.S_IMODE(os.stat(i).st_mode))
+            # Определим установленные разрешения в виде константных чисел
+            # access_arr.append(stat.S_IMODE(os.stat(i).st_mode))
+            # Определим установленные разрешения в виде букв -rwx
+            # access_arr.append(stat.filemode(os.stat(i).st_mode))
+            # Определим установленные разрешения в виде букв -rwx и константных чисел
+            access_arr.append(stat.filemode(os.stat(i).st_mode).ljust(delta, '*') +
+                                # ' '.ljust(delta, ' ') +
+                                # ' ' +
+                                str(stat.S_IMODE(os.stat(i).st_mode)).ljust(delta, '*'))
 
         # Определим установленные разрешения
         # existing_permissions = stat.S_IMODE(os.stat(files_arr[0]).st_mode)
-        existing_permissions = dict(zip(files_arr, access_arr))
+        # existing_permissions = dict(zip(files_arr, access_arr))
+        existing_permissions = dict(zip(files_arr_ljust, access_arr))
 
         # хранение информации о файлах и папках
         output_str = str()
         # заполнеяем хранение информации о файлах и папках
         for k, v in existing_permissions.items():
-            output_str += f'{k} : {v}\n'
+            # output_str += f'{k} : {v}\n'
+            output_str += f'{k}{v}\n'
 
         # вывод информации о файлах и папках
         # self.ids.text_input_main.text = '\n'.join(files_arr)
         # self.ids.text_input_main.text = str(existing_permissions)
-        self.ids.text_input_main.text = str(output_str)
-
-        
+        self.ids.text_input_main.text = str(output_str)  
     # ---------------------------------------------------------------------------
     pass
     # ---------------------------------------------------------------------------
